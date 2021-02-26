8f8c7446c0add6c5f84f8c86f141ae742788f190
ivanangelovski <ivan.angelovski@intecsystem.com>
2021-02-26 10:46:27 +0100

    a

--- src/main/java/com/intec/jokes/MainController.java
+++ src/main/java/com/intec/jokes/MainController.java
@@ -1,17 +1,25 @@
 package com.intec.jokes;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
 @RequestMapping("/")
+@Slf4j
 public class MainController {
 
-     @GetMapping("/home")
+     @GetMapping("/")
      @ResponseBody
-     public String index() {
+     public String index(HttpServletRequest request) {
+          log.info("in home control     local: " + request.getLocalAddr());
+          log.info("in home control    remote: " + request.getRemoteAddr());
           return "hello";
      }
 }
