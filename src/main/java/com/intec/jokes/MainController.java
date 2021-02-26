e564e00419e3db093ff7fd818a7aa5b102f6d768
ivanangelovski <ivan.angelovski@intecsystem.com>
2021-02-26 10:33:56 +0100

    a

--- src/main/java/com/intec/jokes/MainController.java
+++ src/main/java/com/intec/jokes/MainController.java
@@ -3,12 +3,14 @@ package com.intec.jokes;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
 
      @GetMapping("/home")
+     @ResponseBody
      public String index() {
           return "hello";
      }
